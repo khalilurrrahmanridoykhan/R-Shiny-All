a71ef43769688e6b9860eadb3cec33d78ff1baaf
khalilurrrahmanridoykhan <khalilurrahmanridoykhan@gmail.com>
2024-09-25 21:07:31 +0600

    Again

--- app.R
+++ app.R
@@ -4,39 +4,31 @@ library(shinydashboard)
 ui <- dashboardPage(
   dashboardHeader(
     title = "First Dashbord",
-    dropdownMenu(type = "messages",
-             messageItem(
-               from = "Sales Dept",
-               message = "Sales are steady this month."
-             ),
-             messageItem(
-               from = "New User",
-               message = "How do I register?",
-               icon = icon("question")
-             )
-)
+    dropdownMenuOutput("messages")
   ),
   dashboardSidebar(
     sidebarMenu(
-        menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-        menuItem("Widgets", tabName = "widgets", icon = icon("th")),
-        menuItem("Charts", tabName = "charts", icon = icon("bar-chart-o"))
+      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("Widgets", tabName = "widgets", icon = icon("th")),
+      menuItem("Charts", tabName = "charts", icon = icon("bar-chart-o"))
     )
   ),
   dashboardBody(
     tabItems(
       # First tab content
-      tabItem(tabName = "dashboard",
+      tabItem(
+        tabName = "dashboard",
         fluidRow(
           box(plotOutput("plot1", height = 250)),
-
           box(
             title = "Controls",
-            sliderInput("slider",
-"Number of observations:",
-1,
-100,
-50)
+            sliderInput(
+              "slider",
+              "Number of observations:",
+              1,
+              100,
+              50
+            )
           )
         )
       ),
@@ -46,24 +38,36 @@ ui <- dashboardPage(
       ),
       tabItem(
         tabName = "charts",
-        div(class = "my-class",p("Charts tab content")),
-        div(class = "input-filed", textInput("text",
-"Text input:")),
+        div(class = "my-class", p("Charts tab content")),
+        div(class = "input-filed", textInput(
+          "text",
+          "Text input:"
+        )),
       )
     )
   )
 )
 
-
+dropdownMenu(
+  type = "messages",
+  messageItem(
+    from = "Sales Dept",
+    message = "Sales are steady this month."
+  ),
+  messageItem(
+    from = "New User",
+    message = "How do I register?",
+    icon = icon("question")
+  )
+)
 
 server <- function(input, output) {
-    set.seed(122)
-    histdata <- rnorm(500)
+  set.seed(122)
+  histdata <- rnorm(500)
 
-    output$plot1 <- renderPlot({
-        data <- histdata[seq_len(input$slider)
-    ]
-        hist(data)
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
   })
 }
 
