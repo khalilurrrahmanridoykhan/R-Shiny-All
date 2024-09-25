9443cb796473936a884809796033083629f21ba2
khalilurrrahmanridoykhan <khalilurrahmanridoykhan@gmail.com>
2024-09-25 20:41:02 +0600

    again

--- app.R
+++ app.R
@@ -3,12 +3,24 @@ library(shinydashboard)
 
 ui <- dashboardPage(
   dashboardHeader(
-    title = "First Dashbord"
+    title = "First Dashbord",
+    dropdownMenu(type = "messages",
+             messageItem(
+               from = "Sales Dept",
+               message = "Sales are steady this month."
+             ),
+             messageItem(
+               from = "New User",
+               message = "How do I register?",
+               icon = icon("question")
+             )
+)
   ),
   dashboardSidebar(
     sidebarMenu(
         menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-        menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+        menuItem("Widgets", tabName = "widgets", icon = icon("th")),
+        menuItem("Charts", tabName = "charts", icon = icon("bar-chart-o"))
     )
   ),
   dashboardBody(
@@ -20,26 +32,39 @@ ui <- dashboardPage(
 
           box(
             title = "Controls",
-            sliderInput("slider", "Number of observations:", 1, 100, 50)
+            sliderInput("slider",
+"Number of observations:",
+1,
+100,
+50)
           )
         )
       ),
       tabItem(
         tabName = "widgets",
         h2("Widgets tab content")
+      ),
+      tabItem(
+        tabName = "charts",
+        div(class = "my-class",p("Charts tab content")),
+        div(class = "input-filed", textInput("text",
+"Text input:")),
       )
     )
   )
 )
+
+
 
 server <- function(input, output) {
     set.seed(122)
     histdata <- rnorm(500)
 
     output$plot1 <- renderPlot({
-        data <- histdata[seq_len(input$slider)]
+        data <- histdata[seq_len(input$slider)
+    ]
         hist(data)
-    })
- }
+  })
+}
 
 shinyApp(ui, server)
